d4e72b662f7e1deb5fbbfceb2359dc7fdc2154c5
eman-ijaz7vals <eman.ijaz@7vals.com>
2019-07-22 18:28:07 +0500

    Controller's test cases done

--- Gemfile
+++ Gemfile
@@ -39,12 +39,19 @@ gem 'bootstrap-sass'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-group :development, :test do
+
+gem 'web-console', group: :development
+
+group :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'rspec-rails', '~> 3.4'
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+  gem 'capybara'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
+  
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'end
--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,7 +1,5 @@
 class IssuesController < ApplicationController
-
   def index
-    # @issues = Project.find(params[:project_id]).issues.all
     @issues = Issue.all
   end
 
@@ -10,18 +8,17 @@ class IssuesController < ApplicationController
   end
 
   def show
-     @issue = Issue.find(params[:id])
+    @issue = Issue.find(params[:id])
   end
 
-
   def edit
-     @issue = Issue.find(params[:id])
+    @issue = Issue.find(params[:id])
   end
 
   def update
     @issue = Issue.find(params[:id])
-    if(@issue.update(issue_params))
-      flash[:update_issue] = "Issue Updated successfully!"
+    if @issue.update(issue_params)
+      flash[:update_issue] = 'Issue Updated successfully!'
       redirect_to @issue
     else
       render 'edit'
@@ -29,35 +26,27 @@ class IssuesController < ApplicationController
   end
 
   def create
-    #render plain: params[:user].inspect
     @issue = Issue.new(issue_params)
     @issue.company_id = 1
     @issue.project_id = 1
     @issue.creator_id = 2
-    # @issue.assignee_id = 2
     @issue.parent_issue_id = 1
-    # @issue.issue_state_id = 2
-    # @issue.issue_type_id = 2
-    if(@issue.save)
-      flash[:save_issue] = "Issue Created successfully!"
-      redirect_to @issue  ## redirect to show page 
+    if @issue.save
+      flash[:save_issue] = 'Issue Created successfully!'
+      redirect_to @issue
     else
       render 'new'
     end
   end
 
-
   def destroy
     @issue = Issue.find(params[:id])
     @issue.destroy
-    flash[:destroy_issue] = "Issue Deleted successfully"
+    flash[:destroy_issue] = 'Issue Deleted successfully'
     redirect_to issues_path
   end
 
   private def issue_params
-    params.require(:issue).permit(:title,:description, :start_date, :due_date, :progress, :priority,
-      :company_id, :creator_id, :assignee_id, :parent_issue_id,:project_id,:issue_state_id,:issue_type_id)
-
-
+    params.require(:issue).permit(:title, :description, :start_date, :due_date, :progress, :priority, :company_id, :creator_id, :assignee_id, :parent_issue_id, :project_id, :issue_state_id, :issue_type_id)
   end
 end
