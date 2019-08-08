07d5f872d117d56b34106627d890c8c16d424657
Abubakar Azeem <abubakar.azeem@7vals.com>
2019-08-08 18:41:08 +0500

    renamed helper method and moved helper methods from reports helper to issues helper

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,28 +1,5 @@
 module ReportsHelper
-  BADGES = ['success', 'warning', 'danger']
-
   def issue_priority(issue)
     Issue::PRIORITY.key(issue.priority)
-  end
-
-  def attribut_title_from_key(key)
-    if key == 'assignee_id'
-      label = 'Assignee'
-    else
-      label = key.titleize
-    end
-    label
-  end
-
-  def assignee_id_key?(key)
-    key == 'assignee_id'
-  end
-
-  def display_user_name(id)
-    current_tanant.users.find(id).name
-  end
-
-  def badge_class_from_priority(priority)
-    BADGES[priority]
   end
 end
