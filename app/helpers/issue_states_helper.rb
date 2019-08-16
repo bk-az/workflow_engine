256edba2b19a1dbdbd76bf50f9214a79abeef0bd
Abubakar Azeem <abubakar.azeem@7vals.com>
2019-08-16 16:10:39 +0500

    added helper method

--- app/helpers/issue_states_helper.rb
+++ app/helpers/issue_states_helper.rb
@@ -6,4 +6,13 @@ module IssueStatesHelper
   def issue_state_table_row_class(issue_state)
     issue_state.issue_id.nil? ? 'global' : "issue-#{issue_state.issue_id}"
   end
+
+  def issue_state_issues_count(issues_count, issue_state)
+    if issues_count.nil?
+      result = issue_state.issues.count
+    else
+      result = issues_count.key?(issue_state.id) ? issues_count[issue_state.id] : 0
+    end
+    result
+  end
 end
