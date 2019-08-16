5ea2a0d15ffbe95f4131743b5de04c80319728a6
Abubakar Azeem <abubakar.azeem@7vals.com>
2019-08-16 13:21:25 +0500

    removed helper

--- app/helpers/issue_types_helper.rb
+++ app/helpers/issue_types_helper.rb
@@ -6,8 +6,4 @@ module IssueTypesHelper
   def issue_type_table_row_class(issue_type)
     issue_type.project_id.nil? ? 'global' : "project-#{issue_type.project_id}"
   end
-
-  def issue_type_project_number(issue_type)
-    issue_type.project_id.nil? ? 'NA' : link_to(project_path(issue_type.project_id)) {'<i class="fas fa-external-link-square-alt"></i>'}
-  end
 end
