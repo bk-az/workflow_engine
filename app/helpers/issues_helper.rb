ddca1576eec5dd085d80fb5ca8f7021f2e8f8d95
Abubakar Azeem <abubakar.azeem@7vals.com>
2019-08-17 10:48:37 +0500

    updated helper methods

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,6 +1,9 @@
 # issues_helper.rb
 module IssuesHelper
-  BADGES = ['success', 'warning', 'danger']
+  BADGES = ['success', 'warning', 'danger'].freeze
+  KEY_TO_CLASS_MAPPINGS = { assignee_id: User,
+                            issue_state_id: IssueState,
+                            issue_type_id: IssueType }.freeze
 
   def get_priority(priority)
     Issue::PRIORITY.key(priority)
@@ -10,21 +13,19 @@ module IssuesHelper
     Issue::PRIORITY
   end
 
-  def attribute_title_from_key(key)
-    if key == 'assignee_id'
-      label = 'Assignee'
+  def get_readable_information(key, value)
+    if value.blank?
+      result = 'NULL'
+    elsif KEY_TO_CLASS_MAPPINGS.keys.include?(key.to_sym)
+      result = KEY_TO_CLASS_MAPPINGS[key.to_sym].find(value).name
+    elsif key == 'progress'
+      result = number_to_percentage(value, precision: 0)
+    elsif key == 'priority'
+      result = get_priority(value)
     else
-      label = key.titleize
+      result = value
     end
-    label
-  end
-
-  def assignee_id_key?(key)
-    key == 'assignee_id'
-  end
-
-  def display_user_name(id)
-    User.find(id).name
+    result
   end
 
   def badge_class_from_priority(priority)
