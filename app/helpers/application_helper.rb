1df428056260bcc82bd5b076c49aae41bbc2a1c2
abdullah-siddiqui-git <muhammad.abdullah@7vals.com>
2019-08-20 13:59:14 +0500

    Resolved final review comments.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 module ApplicationHelper
+  WITHOUT_TEXTURED_BACKGROUND_PAGES_PATHS = [
+    '/' # Homepage
+  ].freeze
+
   def main_layout_class
     user_signed_in? ? 'col-lg-12' : 'offset-lg-1 col-lg-10 offset-md-1 col-md-10 col-sm-12'
   end
 
   def main_body_class_provider
-    if !user_signed_in? && request.path != '/'
-      'textured-background'
-    end
+    'textured-background' if !user_signed_in? && WITHOUT_TEXTURED_BACKGROUND_PAGES_PATHS.include?(request.path)
   end
 end
